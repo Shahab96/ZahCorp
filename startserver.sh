a3f4c0bb46f2c9825d46f53be41b723892a805a0
Shahab Dogar <shahab96@gmail.com>
2021-09-10 23:44:53 -0700

    Revert "v0.0.6"

--- startserver.sh
+++ startserver.sh
@@ -1,1 +1,1 @@
-java -Xms2G -Xmx12G -XX:PermSize=256m -Dfml.read.Timeout=560 -jar forge-1.12.2-14.23.5.2855.jar nogui
+java -Xms2G -Xmx12G -XX:PermSize=256m -Dfml.read.Timeout=560 -jar forge-1.12.2-14.23.5.2855.jar nogu
